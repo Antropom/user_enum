a419e7b350a56b17d7b15994a49f6a88c4cecbd0
Louis C. <60040638+Antropom@users.noreply.github.com>
2025-03-29 14:21:17 +0100

    Update user_enum.py

--- user_enum.py
+++ user_enum.py
@@ -7,6 +7,11 @@ url = sys.argv[1]
 # User list
 list_path = sys.argv[2]
 
+if not url or not list_path:
+    sys.exit()
+
+print(f'Starting enum on {url}')
+
 try :
     with open(list_path, 'r') as list:
         for line in list:
@@ -19,10 +24,11 @@ try :
                 'password': 'password'
             }
 
+
             response = requests.post(url, data=data)
 
             if 'Wrong password' in response.text:
-                print(f'Unsername found: {username}')
+                print(f'Username found: {username}')
             else:
                 continue
 
